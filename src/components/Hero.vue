91022391d4a72006c68427f9ee7cb6d76351a64c
Pandakie <78358953+YSchilling@users.noreply.github.com>
2023-11-19 02:35:16 +0100

    hero section grid 12 rows again

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,14 +3,12 @@ import FancyButton from "./FancyButton.vue";
 </script>
 
 <template>
-  <section class="min-h-screen grid grid-rows-3 grid-cols-1 lg:grid-cols-12">
-    <router-link
-      to="/linktree"
-      class="lg:col-start-10 self-start justify-self-end mt-24 mr-6 lg:mr-0"
+  <section class="min-h-screen grid grid-rows-3 grid-cols-12">
+    <router-link to="/linktree" class="col-start-10 mt-24"
       ><FancyButton>Linktree</FancyButton></router-link
     >
     <h1
-      class="text-4xl md:text-5xl xl:text-6xl row-start-2 justify-self-center lg:justify-self-start lg:col-start-3 col-span-8 leading-tight flex items-center text-center lg:text-left"
+      class="text-4xl md:text-5xl xl:text-6xl row-start-2 justify-self-center lg:justify-self-start col-start-3 col-span-8 leading-tight flex items-center text-center lg:text-left"
     >
       <b>
         Hey! I'm <span id="title">Yorick Schilling</span><br />
