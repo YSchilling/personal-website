f00c2837a44a79bdaac3d31b59b9d9b7eaa61896
YSchilling <78358953+YSchilling@users.noreply.github.com>
2023-11-23 05:07:40 +0100

    made a small description to click on the sketches

--- src/views/Sketches.vue
+++ src/views/Sketches.vue
@@ -5,17 +5,13 @@ import SketchesGrid from "../components/SketchesGrid.vue";
 
 <template>
   <section class="grid grid-cols-12">
-    <Navbar
-      class="col-start-9 col-span-3 lg:col-start-9 lg:col-span-2 justify-self-end my-24 lg:mt-24"
-    />
+    <Navbar class="col-start-9 col-span-3 lg:col-start-9 lg:col-span-2 justify-self-end my-24 lg:mt-24" />
     <h1
-      class="text-4xl md:text-5xl xl:text-6xl row-start-2 justify-self-center lg:justify-self-start col-start-3 col-span-8 leading-tight flex items-center text-center lg:text-left"
-    >
+      class="text-4xl md:text-5xl xl:text-6xl row-start-2 justify-self-center lg:justify-self-start col-start-3 col-span-8 leading-tight flex items-center text-center lg:text-left">
       <b>My <span id="title">Creative Coding</span> Sketches</b>
     </h1>
-    <SketchesGrid
-      class="row-start-3 col-start-2 col-span-10 lg:col-start-3 lg:col-span-8"
-    />
+    <h2 class="row-start-3 col-start-3 col-span-8 font-roboto">// Click on one of the gifs to view the full sketch!</h2>
+    <SketchesGrid class="row-start-4 col-start-2 col-span-10 lg:col-start-3 lg:col-span-8" />
   </section>
 </template>
 
@@ -25,6 +21,6 @@ import SketchesGrid from "../components/SketchesGrid.vue";
 }
 
 section {
-  grid-template-rows: 20vh 20vh 1fr;
+  grid-template-rows: 20vh 20vh 10vh 1fr;
 }
 </style>
