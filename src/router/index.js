76c60cb33e7a1bd31c22600c37004367e52891a7
yschilling <78358953+YSchilling@users.noreply.github.com>
2023-12-29 01:38:40 +0100

    made router jump to top after navigation

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ import Sketches from "../views/Sketches.vue";
 import Impressum from "../views/Impressum.vue";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
