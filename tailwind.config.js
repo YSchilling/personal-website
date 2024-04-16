50786208e5af83cdb1d4828436e367f796ff49ad
YSchilling <78358953+YSchilling@users.noreply.github.com>
2024-04-16 14:09:08 +0200

    updated vite and now using ems modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+export default {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
