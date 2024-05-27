e0edb00ef635fd07b424fcab1b4a258b047c311b
YSchilling <78358953+YSchilling@users.noreply.github.com>
2024-05-27 23:59:29 +0200

    added loading curtain

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const loadingCurtain = ref();
+
+function removeCurtain() {
+  loadingCurtain.value.classList.toggle("movedCurtain");
+}
+
+window.addEventListener("load", removeCurtain);
+</script>
 
 <template>
+  <div
+    ref="loadingCurtain"
+    class="w-screen h-screen fixed top-0 left-0 z-50 transition-all duration-1000"
+    style="background-color: var(--highlight-color)"></div>
   <router-view />
 </template>
 
-<style scoped></style>
+<style scoped>
+.movedCurtain {
+  top: -100%;
+}
+</style>
